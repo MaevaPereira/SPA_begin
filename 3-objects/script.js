0cd135062a957048d6edab24436a918128ee4bdd
Maeva Pereira <maeva.pereira@le-campus-numerique.fr>
2026-04-22 16:30:41 +0200

    3-objects

--- 3-objects/script.js
+++ 3-objects/script.js
@@ -1,3 +1,4 @@
+
 //3.1 Object.keys
 
 const bus = {
@@ -12,6 +13,58 @@ const bus = {
     equipments: ['sae', 'tft', 'speech']
 };
 
+function afficherClesValeurs(obj, prefixe = '') {
+    for (let cle in obj) {
+        if (obj.hasOwnProperty(cle)) {
+            let valeur = obj[cle];
+            let chemin = prefixe ? `${prefixe}.${cle}` : cle;
+
+            if (typeof valeur === 'object' && valeur !== null) {
+                // Appel récursif pour les sous-objets
+                afficherClesValeurs(valeur, chemin);
+            } else {
+                // Affichage de la valeur primitive
+                console.log(`${chemin} : ${valeur}`);
+            }
+        }
+    }
+}
+afficherClesValeurs(bus);
+
+/*
+//sans récursivité
+Object.keys(bus).forEach((key) => {
+    if(typeof bus[key] === "object") {
+        Object.keys(bus[key]).forEach((subKey) => {
+            console.log(subKey + ": " + bus[key][subKey]);
+        })
+    } else {
+        console.log(key + ": " + bus[key]);
+    }
+})
+
+//old version
+Object.keys(bus).forEach((key) => {
+    console.log(key +": "+bus[key] );
+})
+*/
+
 //3.2 Object.values
 
-console.log(data);
+function afficherPerturbations(obj, prefixe = '') {
+    for (let cle in obj) {
+        if (obj.hasOwnProperty(cle)) {
+            let valeur = obj[cle];
+            let chemin = prefixe ? `${prefixe}.${cle}` : cle;
+
+            if (typeof valeur === 'object' && valeur !== null) {
+                // Appel récursif pour les sous-objets
+                afficherPerturbations(valeur, chemin);
+            } else if(cle === 'texte') {
+                // Affichage de la valeur primitive
+                console.log(`${chemin} : ${valeur}`);
+            }
+        }
+    }
+}
+afficherPerturbations(data)
